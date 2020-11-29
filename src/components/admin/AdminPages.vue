fca9e0ba6f2943df2166eff2f754e949e663b0fc
Gustavo Nunes Lucena <gustavo.nunes@codificar.com.br>
2020-11-29 19:55:32 -0300

    Adicinado item nas configurações

--- src/components/admin/AdminPages.vue
+++ src/components/admin/AdminPages.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="admin-pages">
-      <PageTitle icon="fa fa-cogs" title="Configurações" subTitle="Informações gerais do sistema" />
+    <PageTitle
+      icon="fa fa-cogs"
+      title="Configurações"
+      subTitle="Informações gerais do sistema"
+    />
+
+    <div class="cont-config">
+      <toggle-button
+        :value="toggleBannerActive"
+        color="#40c757"
+        :sync="true"
+        :labels="true"
+        @change="toggleBanner"
+      />
+      <span class="span-text-config">Exibir Banner tela inicial do Aplicativo</span>
+    </div>
+
+
   </div>
 </template>
 
 <script>
-import PageTitle from '../template/PageTitle'
+import PageTitle from "../template/PageTitle"
 
 export default {
-    name: 'AdminPages',
-    components: { PageTitle }
+  name: "AdminPages",
+  components: { PageTitle },
+  data() {
+    return {
+      toggleBannerActive: false
+    }
+  },
+  methods: {
+    toggleBanner() {
+      this.toggleBannerActive = !this.toggleBannerActive
+    }
+  }
 }
 </script>
 
 <style>
+
+.cont-config {
+  display: flex;
+  align-items: flex-start;
+}
+
+.span-text-config {
+  margin-left: 15px;
+  color: #808080;
+}
 
 </style>
--- src/config/bootstrap.js
+++ src/config/bootstrap.js
@@ -3,6 +3,8 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import Vue from 'vue'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import ToggleButton from 'vue-js-toggle-button'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(ToggleButton)
